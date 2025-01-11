0aa9728a8bb05ea1851c0391a0ebf598b58e225b
rochayuri.dev@gmail.com <rochayuri.dev@gmail.com>
2025-01-11 19:25:43 -0300

    Alterações finais no CORS

--- app.js
+++ app.js
@@ -17,10 +17,21 @@ const port = 5000;
 
 //#region middlewares
 
-app.use(cors({
-    origin: "https://www.missaoelderrocha.com.br",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+const allowedOrigins = [
+    "https://www.missaoelderrocha.com.br",
+    "http://www.uptimerobot.com"
+  ];
+  
+  app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
 }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
